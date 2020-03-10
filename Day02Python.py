df2e804e1e92a3aa16f59dccd0808a9ea751053b
CAMERON LUYT <cameronluyt30@gmail.com>
2020-03-10 15:09:32 +0200

    Day02Python01

---
+++ Day02Python.py
@@ -0,0 +1,18 @@
+strSentence = "Lifechoice Coding Academy"
+print(type(strSentence))
+print(strSentence[0:4])
+#slicing
+num1 = 25
+num2 = 50.5
+num3 = 3e17j
+print(type(num1),type(num2),type(num3))
+# int float complex
+mylist = [65, 25, 125, 78, 120, 33]
+mylist.sort()
+mylist.reverse()
+mylist.insert(0,37)
+print(mylist)#tuple
+cars = ("Datsun", "Ford", "BMW", "VW")
+print(type(cars))
+xdict = {"1":"Amy", "2":"Tania","3":"Nathan"}
+print(type(xdict))#dict
