dd0735c6c4d8e373f2518d3c7146017da3e6ad35
CAMERON LUYT <cameronluyt30@gmail.com>
2020-03-17 15:58:45 +0200

    17Mrch2020

---
+++ 17March2020.py
@@ -0,0 +1,24 @@
+cars = ["BMW", "VW", "FORD", "Nissan"]
+
+for x in cars :
+    print(x)
+    if x == "VW":
+        continue
+        print(x)
+
+bikes = ["yamaha", "honda", "bmw", "kawasaki"]
+for b in bikes:
+    if b == "bmw":
+        continue
+    print(b)
+    print(x, b)
+    for x in cars :
+     for b in bikes :
+         print(x, b)
+
+for number in range(2, 21, 2):#range[counting in 2's]
+    print(number)
+
+else:
+    print("we finished lessons on loops")
+
